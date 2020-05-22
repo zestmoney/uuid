a581b7ab16b7b7c8035d3ea6255aac673d84549e
Abhishek Kundalia <abhishek@zestmoney.in>
2020-05-22 10:15:10 +0530

    added Null UUID

--- sql.go
+++ sql.go
@@ -57,3 +57,31 @@ func (uuid *UUID) Scan(src interface{}) error {
 func (uuid UUID) Value() (driver.Value, error) {
 	return uuid.String(), nil
 }
+
+// NullUUID can be used with the standard sql package to represent a
+// UUID value that can be NULL in the database
+type NullUUID struct {
+	UUID  UUID
+	Valid bool
+}
+
+// Value implements the driver.Valuer interface.
+func (u NullUUID) Value() (driver.Value, error) {
+	if !u.Valid {
+		return nil, nil
+	}
+	// Delegate to UUID Value function
+	return u.UUID.Value()
+}
+
+// Scan implements the sql.Scanner interface.
+func (u *NullUUID) Scan(src interface{}) error {
+	if src == nil {
+		u.UUID, u.Valid = Nil, false
+		return nil
+	}
+
+	// Delegate to UUID Scan function
+	u.Valid = true
+	return u.UUID.Scan(src)
+}
