e40f4316a777cf6f49f49dee01340f8fc9eb32ee
Abhishek Kundalia <abhishek@zestmoney.in>
2020-05-22 13:12:01 +0530

    fixed sql value to send nil if empty uuid

--- sql.go
+++ sql.go
@@ -55,6 +55,9 @@ func (uuid *UUID) Scan(src interface{}) error {
 // transparently. Currently, UUIDs map to strings. Please consult
 // database-specific driver documentation for matching types.
 func (uuid UUID) Value() (driver.Value, error) {
+	if uuid == Nil {
+		return nil, nil
+	}
 	return uuid.String(), nil
 }
 
